e2520a0eea25b86575cf9507b02e84829901807d
Corin <corin.mulliss@gmail.com>
2020-02-10 09:21:51 +0000

    Updates

---
+++ precache-manifest.b473c2265fb7ddd01e395fdbecd09cc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca221b1619bae96a9b80f9801af423f",
+    "url": "/jet-lag/index.html"
+  },
+  {
+    "revision": "d42a3901881d4630622b",
+    "url": "/jet-lag/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "3c6d871eb3d9c8cb7fad",
+    "url": "/jet-lag/static/css/main.adc86e84.chunk.css"
+  },
+  {
+    "revision": "d42a3901881d4630622b",
+    "url": "/jet-lag/static/js/2.b2ed9b58.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/jet-lag/static/js/2.b2ed9b58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c6d871eb3d9c8cb7fad",
+    "url": "/jet-lag/static/js/main.65503c1d.chunk.js"
+  },
+  {
+    "revision": "c01e4aa2a335e1c52447",
+    "url": "/jet-lag/static/js/runtime-main.98880855.js"
+  }
+]);
--- src/components/Lookup/JoiningText.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import styles from "./Lookup.module.scss";
-
-const JoiningText: React.FC<{ text: string }> = ({ text }) => (
-  <div className={styles.joiningText}>{text}</div>
-);
-
-export default JoiningText;
